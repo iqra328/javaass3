01a909ce27695ab0a30100440f2a242edcc8b3b2
iqra328 <rehmaniqra22@gmail.com>
2025-05-29 20:40:40 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// var age = "Iam 15 years old";
+// alert ("Iam 15 years old");
+
+// let visitcount = localStorage.getItem(visitcount);
+// if (visitcount === null) {
+//     visitcount = 1;
+// }
+//  else {
+//     visitcount = parseInt (visitcount) + 1;
+//  }
